bcd5fca722639f62e3acdda1d40a9f26e93ba486
roly24 <rolyartica24@gmail.com>
2022-04-11 17:55:00 +0200

    esercizio d1 w3

---
+++ m1/w3/d1/script.js
@@ -0,0 +1,67 @@
+
+
+/*tipi di test0 */
+
+
+
+
+// object lista di dati e funzioni separate
+// array lista di valori identificati per numeri
+
+
+alert('Hello World!')
+
+
+var etaUtente = 5 ; //dato di tipo numerico 
+var testo = '5' ; //dato di tipo testuale
+var boolean = true ; //
+var oggetto = {
+        nome: 'Mario',
+        cognome: 'Mossi',
+        eta: 30
+};
+var array = [
+    1,
+    2,
+    3,
+    4,
+]
+
+// concatenamento e operazioni tra variabili //
+
+var a = 2;
+var b = 2;
+var c = a + b; //valore 4
+
+var salute = 'ciao';
+var domanda = 'come stai?';
+
+document.write(sauto + '  ' + domanda)
+
+
+document.write('<ul>');
+document.write('<li>' + oggetto.nome + '</li>');
+document.write('<li>' + oggetto.cognome + '</li>');
+document.write('<li>' + oggetto.eta + '</li>');
+document.write('</ul>');
+
+
+/*funzioni */
+
+function somma(){
+
+    document.write(2 + 6);
+}
+
+somma()
+
+function ottieniSomma(){
+
+    return 2 + 6 ;
+}
+
+console.log(oggetto)
+
+
+
+
