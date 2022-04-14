0e82fdf38d89c77c7d16f523541f7b074e1a938f
roly24 <rolyartica24@gmail.com>
2022-04-14 18:01:29 +0200

    esercizio pomeriggio

---
+++ m1/w3/d4/esercizio pomeriggio/script.js
@@ -0,0 +1,39 @@
+
+function sommaSpeciale (x,y) {
+        x++;
+        x= y*2;
+    return x+y
+}
+function assegnaValori (){
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+
+}
+
+var a,b,c,d,e,f;
+
+
+var res = sommaSpeciale (a,b);
+var res1 = sommaSpeciale (c,d);
+var res2 = sommaSpeciale (e,f);
+console.log(res,res1,res2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
